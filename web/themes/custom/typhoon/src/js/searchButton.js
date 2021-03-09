a32d5a843d1f47e002f0a61db684152c4a3ff8b4
Mitch Steimel <steimel@iastate.edu>
2021-03-09 14:49:56 -0600

    Search input slides open

--- web/themes/custom/typhoon/src/js/searchButton.js
+++ web/themes/custom/typhoon/src/js/searchButton.js
@@ -1,9 +1,10 @@
+import { slideToggle } from './slide.js';
 
 const searchButton = document.querySelector('.js-main-search-button');
 
 function handleSearchButtonClick(e) {
-  const searchInput = document.querySelector('.js-search-wrapper')
-  searchInput.classList.toggle('hidden');
+  const searchInput = document.querySelector('.js-search-wrapper');
+  slideToggle(searchInput);
 }
 
 searchButton.addEventListener('click', handleSearchButtonClick);
